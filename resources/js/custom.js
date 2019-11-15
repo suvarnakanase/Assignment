6deab1bc9deb41a3cd54d8e219a49e94c194c8de
suvarnakanase <57622461+suvarnakanase@users.noreply.github.com>
2019-11-15 22:08:57 +0530

    Add files via upload

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -41,10 +41,16 @@ var theme = function () {
 }();
 
 
-
-$(document).ready(function(){
-   var currentttl = $('#side-nav .current').text();
+$(window).load(function(){
+       var currentttl = $('#side-nav .current').text();
     $('.sidebar .title').html(currentttl);
+    $('#side-nav li').click(function(){
+        
+         var currentttl1 = $(this).text();
+       // alert("currentttl1");
+         $('.sidebar .title').html(currentttl1);
+      });
+    
     $('#side-nav').onePageNav({
 	currentClass: 'current',
 	changeHash: false,
@@ -53,16 +59,37 @@ $(document).ready(function(){
 	filter: '',
 	easing: 'swing',
 	begin: function($currentListItem) {
-		//var val = $currentListItem.find('a').text();
-//       alert(val);
-        //$('.sidebar .title').html(val);
+
 	},
 	end: function($currentListItem) {
-        //var val = $currentListItem.find('a').text();
-//        alert(val);
-//        alert("hi");
-//		$('body').append('<div id="device-dummy" style="height: 1px;"></div>');
-       // $('.sidebar .title').html(val);
+	},
+	scrollChange: function($currentListItem) {
+		var val = $currentListItem.find('a').text();
+      $('.sidebar .title').html(val);
+	}
+});
+});
+$(document).ready(function(){
+   var currentttl = $('#side-nav .current').text();
+    $('.sidebar .title').html(currentttl);
+    $('#side-nav li').click(function(){
+        
+         var currentttl1 = $(this).text();
+       // alert("currentttl1");
+         $('.sidebar .title').html(currentttl1);
+      });
+    
+    $('#side-nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	begin: function($currentListItem) {
+
+	},
+	end: function($currentListItem) {
 	},
 	scrollChange: function($currentListItem) {
 		var val = $currentListItem.find('a').text();
@@ -76,5 +103,60 @@ $('#sidebar').stickySidebar({
     bottomSpacing: 0
 }); 
     
-   
+ $('.custom-radio input').iCheck();  
+    
+$('.menu-btn').click(function(){
+   // alert("hi");
+    $('.nav').slideToggle();
+    $(this).toggleClass('open');
+    $('header').toggleClass('fixed-header');
+    return false;
+})
+    
+
+$('.spinner .spinner-btn').click(function(){
+    var spinnervalue = $(this).parent('.spinner').find('.spin-val').val();
+    //alert(spinnervalue);
+    if($(this).hasClass('increament-btn'))
+        {
+            //alert("increase");
+            $(this).parent('.spinner').find('.spin-val').val(parseInt(spinnervalue)  + 1)
+        }
+    else if(parseInt(spinnervalue)>0){
+         //alert("decrease");
+         $(this).parent('.spinner').find('.spin-val').val(parseInt(spinnervalue) - 1)
+    }
+
+})
+    
+    
+    
+    size_li = $(".review-list-wrap > ul > li").size();
+    var remaing_size = size_li - 2;
+     $('.review-list-wrap a.load_more .more-list').html(remaing_size);
+    //alert(size_li);
+    x=2;
+    $('.review-list-wrap > ul > li:lt('+x+')').show();
+    $('.review-list-wrap a.load_more').click(function () {
+        x= (x+2 <= size_li) ? x+2 : size_li;
+        $('.review-list-wrap > ul > li:lt('+x+')').show();
+        var remaing_size = size_li - x;
+        //console.log(remaing_size);
+        $('.review-list-wrap a.load_more .more-list').html(remaing_size);
+        if(remaing_size == 0){
+            $('.review-list-wrap a.load_more').html("No more items");
+            $('.review-list').addClass('no-shadow');
+        }
+    });
+
+//    if ( $(window).width() < 768){
+//    size_li = $(".review-list-wrap > ul > li").size();
+//    x=6;
+//    $('.review-list-wrap > ul > li:lt('+x+')').show();
+//    $('.tramsaction_bottom a.load_more1').click(function () {
+//        x= (x+3 <= size_li) ? x+3 : size_li;
+//        $('.review-list-wrap > ul > li:lt('+x+')').show();
+//    });
+//}
+
 });
